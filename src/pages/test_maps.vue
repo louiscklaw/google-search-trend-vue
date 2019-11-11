aea0ae70c81ab35566e98268de93dcc7760fbeb3
louiscklaw <louiscklaw@gmail.com>
2019-11-11 10:59:40 +0800

    init test pages,

---
+++ src/pages/test_maps.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  section.section
+    .columns
+      .column
+        font-awesome-icon(:icon="['fas', 'camera']")
+        font-awesome-icon(:icon="['fas', 'external-link-alt']")
+
+
+</template>
+
+<script>
+  import layoutDefault from '../layouts/default.vue'
+
+  export default {
+    created() {
+      this.$emit( 'update:layout', layoutDefault )
+    },
+    metaInfo: {
+      // if no subcomponents specify a metaInfo.title, this title will be used
+      title: 'About',
+      meta: [{
+        vmid: 'description',
+        name: 'description',
+        content: 'about'
+      }]
+    }
+
+  }
+</script>
+
+<style>
+
+</style>
